a956407ba4bf7e806afe684a8b78d5b5ec3c01e8
Akshit Sahani <akshit_sahani@hotmail.com>
2017-04-04 13:19:35 -0400

    added location

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -119,7 +119,7 @@ class VolunteersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def volunteer_params
-      params.require(:volunteer).permit(:first_name, :last_name, :email, :to_learn, :neighborhood, :city, :password, :password_confirmation,
+      params.require(:volunteer).permit(:first_name, :last_name, :email, :to_learn, :neighborhood, :city, :location, :age, :password, :password_confirmation,
       hobbies_attributes: [ :id, :name, :_destroy ],
       languages_attributes: [ :id, :language, :_destroy ],
       cultural_backgrounds_attributes: [ :id, :background, :_destroy],
